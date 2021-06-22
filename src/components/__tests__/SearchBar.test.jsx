87671244b692b0776e6651278d5f78c8a6912c2a
Dan <danko56666@gmail.com>
2021-06-22 19:37:00 -0400

    write test for searchbar input text

---
+++ src/components/__tests__/SearchBar.test.jsx
@@ -0,0 +1,10 @@
+import Searchbar from "../Searchbar";
+import { renderWithThemeProvider, screen, fireEvent } from "../../test-utils";
+
+test("input text renders correctly", () => {
+  renderWithThemeProvider(<Searchbar />);
+
+  const input = screen.getByRole("textbox");
+  fireEvent.change(input, { target: { value: "Cruella" } });
+  expect(input.value).toBe("Cruella");
+});
