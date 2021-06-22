81f99752314103fc7b1e47b871d7dddc609a8677
Dan <danko56666@gmail.com>
2021-06-22 14:49:17 -0400

    add rating banner

--- src/features/movies/MovieItem.jsx
+++ src/features/movies/MovieItem.jsx
@@ -3,10 +3,28 @@ import styled from "styled-components";
 const MovieCardStyled = styled.div`
   height: 348px;
   width: 100%;
+  position: relative;
   background: ${(props) => props.theme.colors.bg.primary};
   border: 1px solid ${(props) => props.theme.colors.bg.secondary};
   border-radius: 8px;
   box-shadow: 1px 2px 8px 2px rgba(0, 0, 0, 0.1);
+`;
+
+const RatingBannerStyled = styled.div`
+  display: flex;
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  height: 34px;
+  width: 34px;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  border: 1px solid ${(props) => props.theme.colors.text.primary};
+  justify-content: center;
+  align-items: center;
+  font-family: ${(props) => props.theme.fontFamily[0]};
+  font-weight: ${(props) => props.theme.fontWeights.medium};
+  font-size: ${(props) => props.theme.fontSizes[0]};
 `;
 
 const CardImageStyled = styled.img`
@@ -28,11 +46,12 @@ const CardFooterStyled = styled.div`
 `;
 
 const MovieItem = ({ movie }) => {
-  const { backdrop_path, title } = movie;
+  const { backdrop_path, title, vote_average } = movie;
   console.log(movie);
 
   return (
     <MovieCardStyled>
+      <RatingBannerStyled>{vote_average}</RatingBannerStyled>
       <CardImageStyled
         src={`${process.env.REACT_APP_API_BASE_IMAGE_URL}${backdrop_path}`}
         alt="backdrop_image"
