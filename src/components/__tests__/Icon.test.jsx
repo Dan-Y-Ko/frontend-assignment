6929b36b7b3079946395bfb0fd6f5114be32daae
Dan <danko56666@gmail.com>
2021-06-23 20:26:49 -0400

    add another test to check if styling gets added to logo correctly

--- src/components/__tests__/Icon.test.jsx
+++ src/components/__tests__/Icon.test.jsx
@@ -5,19 +5,29 @@ import { renderWithThemeProvider, screen } from "../../test-utils";
 
 describe("Icon component", () => {
   test("logo renders correctly", () => {
+    renderWithThemeProvider(<Icon icon="timescale" alt="Timescale" />);
+
+    const image = screen.getByAltText("Timescale");
+
+    expect(image.src).toContain(logo);
+  });
+
+  test("logo styling gets applied correctly", () => {
     renderWithThemeProvider(
       <Icon styles={{ height: "6vh" }} icon="timescale" alt="Timescale" />
     );
 
     const image = screen.getByRole("img");
+    const imageElement = document.getElementsByClassName(image.className);
+    const style = window.getComputedStyle(imageElement[0]);
 
-    expect(image.src).toContain(logo);
+    expect(style.height).toBe("6vh");
   });
 
   test("close icon renders correctly", () => {
     renderWithThemeProvider(<Icon icon="closeIcon" alt="close-icon" />);
 
-    const image = screen.getByRole("img");
+    const image = screen.getByAltText("close-icon");
 
     expect(image.src).toContain(closeIcon);
   });
